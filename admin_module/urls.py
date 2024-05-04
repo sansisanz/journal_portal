28a242d813558aca908b773bb8ad2713f6af85f5
subindax <subindax@gmail.com>
2024-05-04 15:02:47 +0530

    edited again on may 4

--- admin_module/urls.py
+++ admin_module/urls.py
@@ -10,9 +10,13 @@ urlpatterns = [
     path("forgotpassword/", views.forgotpassword),
     path("visits/", views.visits),
     path("downloads/", views.downloads),
-    path("userlist/", views.userlist),
+    path("user_access/", views.user_access),
     path("admin_profile/", views.admin_profile),
     path("admin_resetpassword/", views.admin_resetpassword),
     path("set_password/", views.set_password),
+    path("admin_navbar/", views.admin_navbar),
+    path("create_journal/", views.create_journal),
+    path("view_journals/", views.view_journals),
+    path("visit_counts/", views.visit_counts),
 ]
 
